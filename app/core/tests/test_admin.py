8fcf6b874fd1fd5ee2b53381ac7a505af2a2e7d2
Wanlipa Thongsuntia <nuam18@gmail.com>
2022-12-17 15:45:30 +0700

    Added user

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -22,3 +22,26 @@ class AdminSiteTests(TestCase):
             password='testpass123',
             name='Test User'
         )
+
+    def test_user_list(self):
+        """Test that users are listed on page."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page workd."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
